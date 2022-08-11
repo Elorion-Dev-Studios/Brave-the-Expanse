b5fc6c4b001a4c5508647890e325d4bf9e717dc7
Erin Brown <erinbrown013@gmail.com>
2022-08-11 19:36:41 -0400

    Added player movement from input on X and Y

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,16 +4,28 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField]
+    private float _speed = 3.5f;
+    private Vector3 _direction;
+
+
     // Start is called before the first frame update
     void Start()
     {
         //set starting position to (0,0,0)
         transform.position = Vector3.zero;
+        _direction = Vector3.zero;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //move player based on user input
+        _direction.x = Input.GetAxis("Horizontal");
+        _direction.y = Input.GetAxis("Vertical");
+
+        //up down left right
+        transform.Translate(_direction * _speed * Time.deltaTime);
     }
 }
