688109d20f6c28bc4ef37caf62005b8765b4af1a
Erin Brown <erinbrown013@gmail.com>
2022-08-11 21:29:12 -0400

    Add player movment bounds and X wrapping

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,14 @@ public class Player : MonoBehaviour
     [SerializeField]
     private float _speed = 3.5f;
     private Vector3 _direction;
+    [SerializeField]
+    private float _screenMaxX;
+    [SerializeField]
+    private float _screenMinX;
+    [SerializeField]
+    private float _screenMaxY;
+    [SerializeField]
+    private float _screenMinY;
 
 
     // Start is called before the first frame update
@@ -21,11 +29,35 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+
         //move player based on user input
         _direction.x = Input.GetAxis("Horizontal");
         _direction.y = Input.GetAxis("Vertical");
-
-        //up down left right
         transform.Translate(_direction * _speed * Time.deltaTime);
+
+        Vector3 playerPosition = transform.position;
+
+        //keep player between MaxX, MinX
+        if (playerPosition.x > _screenMaxX)
+        {
+            playerPosition.x = _screenMinX;
+        } 
+        else if (playerPosition.x < _screenMinX)
+        {
+            playerPosition.x = _screenMaxX;
+        }
+
+        //keep player between MaxY, MinY
+        if(playerPosition.y > _screenMaxY)
+        {
+            playerPosition.y = _screenMaxY;
+        }
+        else if (playerPosition.y < _screenMinY)
+        {
+            playerPosition.y = _screenMinY;
+        }
+
+        transform.position = playerPosition;
     }
 }
