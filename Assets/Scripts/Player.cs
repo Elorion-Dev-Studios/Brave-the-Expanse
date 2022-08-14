309ab928a01a7532a47471e66bc9de00dafd6b7f
Erin Brown <erinbrown013@gmail.com>
2022-08-14 17:42:48 -0400

    Added cool down to fire laser behavior

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -22,6 +22,11 @@ public class Player : MonoBehaviour
     [SerializeField]
     private float _laserOffsetY;
     private Vector3 _laserOffsetVector;
+    //minimum time between laser fires
+    [SerializeField]
+    private float _fireRate = 0.5f;
+    private float _nextFire = -1f;
+    
 
 
     void Start()
@@ -36,9 +41,11 @@ public class Player : MonoBehaviour
     {
         CalculateMovement();
 
-        //if space key pressed
-        if (Input.GetKeyDown(KeyCode.Space))
+        //if space key pressed & game run time is greater than cache next fire time
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > _nextFire )
         {
+            // update next fire time with current game run time plus fire rate
+            _nextFire = Time.time + _fireRate;
             // create laser at player position plus offset
             Instantiate(_laserPrefab, (transform.position + _laserOffsetVector), Quaternion.identity);
         }
