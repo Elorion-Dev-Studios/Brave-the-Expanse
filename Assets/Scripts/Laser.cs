9e5646965569f6771f5949147ebd72cc47891cfc
Erin Brown <erinbrown013@gmail.com>
2022-08-14 17:47:17 -0400

    Cleaned up Laser script

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -4,29 +4,23 @@ using UnityEngine;
 
 public class Laser : MonoBehaviour
 {
-    //speed variable
     [SerializeField]
     private float _speed = 5f;
     [SerializeField]
     private float _maxY = 7f;
 
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        //move laser up at speed
+        CalculateMovement();
+    }
+
+    void CalculateMovement()
+    {
         transform.Translate(Vector3.up * _speed * Time.deltaTime);
 
-        //if position is greater than max Y
         if (transform.position.y > _maxY)
         {
-            //destroy laser
             Destroy(this.gameObject);
         }
     }
