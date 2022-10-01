6609c3f14b50cbeb96fe32a7e5d25e0e428aeb91
Erin Brown <erinbrown013@gmail.com>
2022-10-01 13:22:01 -0400

    Adjusted collisions for 2D objects

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -31,7 +31,12 @@ public class Enemy : MonoBehaviour
         }
     }
 
-    private void OnTriggerEnter(Collider other)
+/*    private void OnTriggerEnter(Collider other)
+    {
+
+    }*/
+
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
@@ -54,6 +59,5 @@ public class Enemy : MonoBehaviour
             Destroy(other.gameObject);
             Destroy(this.gameObject);
         }
-
     }
 }
