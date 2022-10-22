272a9a01b828b0ec3fde4e621d5b2553be64603e
Erin Brown <erinbrown013@gmail.com>
2022-10-21 22:56:16 -0400

    Added Credits screen

--- Assets/Scripts/MainMenu/MenuManager.cs
+++ Assets/Scripts/MainMenu/MenuManager.cs
@@ -6,10 +6,31 @@ using UnityEngine.SceneManagement;
 public class MenuManager : MonoBehaviour
 {
 
+    [SerializeField] private GameObject _titleScreenCanvas;
+    [SerializeField] private GameObject _creditsScreenCanvas;
+
+    void Start()
+    {
+        _titleScreenCanvas.SetActive(true);
+        _creditsScreenCanvas.SetActive(false);
+    }
+
     // to be called by New Game button OnClick
     public void NewGame()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void LoadTitleScreen()
+    {
+        _titleScreenCanvas.SetActive(true);
+        _creditsScreenCanvas.SetActive(false);
+    }
+
+    public void LoadCreditsScreen()
+    {
+        _titleScreenCanvas.SetActive(false);
+        _creditsScreenCanvas.SetActive(true);
     }
 
 }
