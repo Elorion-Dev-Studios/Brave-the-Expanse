835e3fd331a34321950bf89a3cc66b3159216893
Erin Brown <erinbrown013@gmail.com>
2023-08-05 13:29:13 -0400

    add quit menu to confirm quit request

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -6,10 +6,21 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     private bool _gameOver = false;
+    private bool _paused = false;
+    private UIManager _uiManager;
+    
+
 
     void Start()
     {
         _gameOver = false;
+
+        _uiManager = GameObject.Find("UIManager").GetComponent<UIManager>();
+        if (_uiManager == null)
+        {
+            Debug.LogError("Player failed to cache reference to UI Manager");
+        }
+
     }
 
 
@@ -25,16 +36,9 @@ public class GameManager : MonoBehaviour
         //quit game
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            //quit play mode in editor
-            if (UnityEditor.EditorApplication.isPlaying)
-            {
-                UnityEditor.EditorApplication.isPlaying = false;
-            }
-            else
-            {
-                //quit application
-                Application.Quit();
-            }
+            PauseGame();
+
+            _uiManager.QuitMenu();
         }
     }
 
@@ -43,4 +47,31 @@ public class GameManager : MonoBehaviour
     {
         _gameOver = true;
     }
+
+    public void PauseGame()
+    {
+        _paused = true;
+        Time.timeScale = 0f;
+    }
+
+    public void ResumeGame()
+    {
+        _paused = false;
+        Time.timeScale = 1.0f;
+    }
+
+    public void QuitGame()
+    {
+        if (UnityEditor.EditorApplication.isPlaying)
+        {
+            UnityEditor.EditorApplication.isPlaying = false;
+        }
+        else
+        {
+            //quit application
+            Application.Quit();
+        }
+
+    }
+
 }
